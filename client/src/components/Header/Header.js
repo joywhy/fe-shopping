6cf3c1d5cecb28a0181f86675869b8d69f4738b9
joywhy <leh9111@naver.com>
2023-06-06 11:53:08 +0900

    Rename: common 폴더 구성 변경

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import Hamburger from '../../common/data/image/Hamburger';
-
+import Hamburger from '../../common/image/Hamburger';
+import logo from '../../common/image/original/logo.svg';
 const Header = () => {
   return (
     <div>
       <div>
-        <img src="" alt="" />
+        <img src={logo} alt="로고" />
         <h1>Coz shopping</h1>
       </div>
       <div>
         <Hamburger />
+        <img src="" alt="햄버거 버튼" />
       </div>
     </div>
   );
