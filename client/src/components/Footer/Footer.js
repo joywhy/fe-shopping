18b239c7d0d75de53ea43eaf20e6a1a0ae57e546
joywhy <leh9111@naver.com>
2023-06-07 23:40:31 +0900

    Fix: 매니페스트 아이콘 오류

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -5,6 +5,7 @@ const Footer = () => {
   return (
     <Container>
       <p>개인정보 처리방침 | 이용 약관</p>
+      {/* 기본적으로 p 태그는 블럭요소인데 디폴트 너비값이 부모너비의 100%가 아니라 해당 text 영역만큼만 차지할까 */}
       <p>All rights reserved @ Codestates</p>
     </Container>
   );
