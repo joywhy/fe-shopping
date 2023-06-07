decc0dac5a678a8406dea0c812dcc7aabf4b8ae4
joywhy <leh9111@naver.com>
2023-06-07 23:29:42 +0900

    Style: 푸터 스타일링

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -2,7 +2,12 @@ import React from 'react';
 import { Container } from './Footer.styled';
 
 const Footer = () => {
-  return <Container>footer</Container>;
+  return (
+    <Container>
+      <p>개인정보 처리방침 | 이용 약관</p>
+      <p>All rights reserved @ Codestates</p>
+    </Container>
+  );
 };
 
 export default Footer;
